b7e03dde78ad8bfc5bfe30e111451a52af131c69
S Nitesh Singh <niteshseram@gmail.com>
2021-04-29 19:13:53 +0530

    adding crossorigin annotation in the controller

--- src/main/java/com/nitesh/ipldashboard/controller/TeamController.java
+++ src/main/java/com/nitesh/ipldashboard/controller/TeamController.java
@@ -4,11 +4,13 @@ import com.nitesh.ipldashboard.model.Team;
 import com.nitesh.ipldashboard.repository.MatchRepository;
 import com.nitesh.ipldashboard.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
